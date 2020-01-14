6aef985df638635ee8b4e4bed6187b06da2e4512
MaciejDziuban <dziuban.maciej@gmail.com>
2020-01-14 22:03:50 +0100

    Set icons

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,14 +1,30 @@
 #include "source/FindIconFiles/FindIconFiles.h"
 #include "source/FindDirectories/FindDirectories.h"
 
-//#include <Shlobj.h>
+#include <Shlobj.h>
 #include <iostream>
+#include <strsafe.h>
 #include <string>
 
+void setIcon(const std::wstring &directory, const std::wstring &iconFile, int iconIndex) {
+    // Copy icon file path to non-const buffer
+    auto iconPath = std::make_unique<wchar_t[]>(iconFile.size() + 1);
+    StringCchCopyW(iconPath.get(), iconFile.size() + 1, iconFile.data());
+
+    SHFOLDERCUSTOMSETTINGS fcs = {};
+    fcs.dwSize = sizeof(SHFOLDERCUSTOMSETTINGS);
+    fcs.dwMask = FCSM_ICONFILE;
+    fcs.pszIconFile = iconPath.get();
+    fcs.cchIconFile = 0;
+    fcs.iIconIndex = iconIndex;
+    HRESULT result = SHGetSetFolderCustomSettings(&fcs, directory.c_str(), FCS_FORCEWRITE);
+    if (result != S_OK) {
+        auto err = GetLastError();
+        throw 1; // TODO handle
+    }
+}
+
 int main() {
-
-    //auto a = findIconFile(L"E:\\Programs\\Free Video Cutter");
-
     auto subdirectories = getSubdirectories(L"E:\\Programs");
     for (const auto &directory : subdirectories) {
         std::wcout << directory << "\t\t\t";
@@ -17,20 +33,11 @@ int main() {
             std::wcout << iconFile << " ";
         }
         std::wcout << "\n";
+
+        if (iconFiles.size() > 0) {
+             setIcon(directory, iconFiles[0], 0);
+        }
     }
 
     return 0;
-
-    /*wchar_t icon[] = L"D:\\Desktop\\icons.ico";
-    auto err = GetLastError();
-    SHFOLDERCUSTOMSETTINGS fcs = {0};
-    fcs.dwSize = sizeof(SHFOLDERCUSTOMSETTINGS);
-    fcs.dwMask = FCSM_ICONFILE;
-    fcs.pszIconFile = icon;
-    fcs.cchIconFile = 0;
-    fcs.iIconIndex = 0;
-    SHGetSetFolderCustomSettings(&fcs, L"D:\\Desktop\\TestDir2", FCS_FORCEWRITE);
-    err = GetLastError();
-
-    return 0;*/
 }
